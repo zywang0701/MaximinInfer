d4939d08c91220decaee13a36dad3d47d1aa7b47
wangzy0701 <wangzy0701@gmail.com>
2021-10-18 00:38:11 -0400

    Use S3
    Transfer to object-oriented programming style.

--- R/source_functions.R
+++ R/source_functions.R
@@ -1,148 +1,3 @@
-#' Inference for a linear combination of regression coefficients in high dimensional linear regression
-#'
-#' @description
-#' Computes the bias corrected estimator of the linear combination of regression coefficients and
-#' the corresponding standard error. It also constructs the confidence interval for the linear combination.
-#'
-#' @param X Design matrix, of dimension \eqn{n} x \eqn{p}
-#' @param y Outcome vector, of length \eqn{n}
-#' @param loading Loading, of length \eqn{p}
-#' @param intercept Should intercept(s) be fitted or not (default = \code{TRUE})
-#' @param init.Lasso Initial LASSO estimator of the regression vector (default = \code{TRUE})
-#' @param lambda The tuning parameter in the construction of LASSO estimator of the regression vector (default = \code{NULL})
-#' @param mu The dual tuning parameter used in the construction of the projection direction (default = \code{NULL})
-#' @param step The step size used to compute \code{mu}; if set to \code{NULL} it is computed to be
-#' the number of steps (\code{maxiter}) to obtain the smallest \code{mu} such that the dual optimization problem
-#' for constructing the projection direction converges (default = \code{NULL})
-#' @param resol The factor by which \code{mu} is increased/decreased to obtain the smallest \code{mu}
-#' such that the dual optimization problem for constructing the projection direction converges (default = 1.5)
-#' @param maxiter Maximum number of steps along which \code{mu} is increased/decreased to obtain the smallest \code{mu}
-#' such that the dual optimization problem for constructing the projection direction converges (default = 6)
-#'
-#' @return
-#' \item{prop.est}{The bias-corrected estimator for the linear combination of regression coefficients}
-#' \item{se}{The standard error of the bias-corrected estimator}
-#' \item{proj}{The projection direction, of length \eqn{p}}
-#' \item{plug.in}{The plug-in LASSO estimator for the linear combination}
-#'
-#' @examples
-#' \dontrun{
-#' n <- 90
-#' p <- 200
-#' A1gen <- function(rho,p){
-#'  A1=matrix(0,p,p)
-#'   for(i in 1:p){
-#'    for(j in 1:p){
-#'      A1[i,j] <- rho^(abs(i-j))
-#'    }
-#'   }
-#'  A1
-#' }
-#' mu <- rep(0,p)
-#' rho <- 0.5
-#' Cov <- (A1gen(rho,p))/2
-#' beta <- rep(0,p)
-#' beta[1:10] <- c(1:10)/5
-#' X <- MASS::mvrnorm(n,mu,Cov)
-#' y <- X%*%beta + rnorm(n)
-#' loading <- c(1,rep(0,(p-1)))
-#' Est <- LF(X = X, y = y, loading = loading, intercept = TRUE)
-#' }
-LF<-function(X,y,loading,intercept=TRUE,init.Lasso=NULL,lambda=NULL,mu=NULL,step=NULL,resol = 1.5,maxiter=10){
-  ### Option 1: search tuning parameter with steps determined by the ill conditioned case (n=p/2)
-  ### Option 2: search tuning parameter with maximum 10 steps.
-  ### Option 3: fixed tuning parameter and this is not recommended without exploring the tuning parameter selection
-  xnew<-loading
-  p <- ncol(X);
-  n <- nrow(X);
-  n_y <- length(y)
-
-  if(n_y!=n)
-  {
-    stop("Error: Check dimensions of X and y")
-  }
-  else
-  {
-    data = na.omit(data.frame(y,X))
-    X <- as.matrix(data[,-1])
-    y <- as.vector(data[,1])
-    p <- ncol(X)
-    n <- nrow(X)
-
-    # implement the correction of the initial estimator
-    # set up the randomization step
-    if (intercept==TRUE){
-      Xc <- cbind(rep(1,n),X);
-      pp <- (p+1);
-    } else {
-      Xc <- X;
-      pp <- p
-    }
-    if(is.null(init.Lasso)){
-      # implement a lasso algorithm to get beta and sigma
-      init.Lasso<-Initialization.step(X,y,lambda,intercept)
-      htheta<-init.Lasso$lasso.est
-    }else{
-      htheta<-init.Lasso
-    }
-    sparsity <- sum(abs(htheta) > 0.001)
-    sd.est <- sqrt(sum((y - Xc %*% htheta)^2) / n)
-
-    # compute the initial estimator
-    if(intercept==TRUE){
-      loading=rep(0,pp)
-      loading[1]=1
-      loading[-1]=xnew
-    }else{
-      loading=xnew
-    }
-    loading.norm<-sqrt(sum(loading^2))
-    lasso.plugin<-sum(loading*htheta)
-
-    #############################################
-    ############# Correction step ###############
-
-    if ((n>=6*p)){
-      sigma.hat <- (1/n)*(t(Xc)%*%Xc);
-      tmp <- eigen(sigma.hat)
-      tmp <- min(tmp$values)/max(tmp$values)
-    }else{
-      tmp <- 0
-    }
-    sigma.hat <- (1/n)*(t(Xc)%*%Xc);
-    if ((n>=6*p)&&(tmp>=1e-4)){
-      direction <- solve(sigma.hat)%*%loading/loading.norm
-    }else{
-      if(is.null(step)){
-        step.vec<-rep(NA,3)
-        for(t in 1:3){
-          index.sel<-sample(1:n,size=ceiling(0.5*min(n,p)), replace=FALSE)
-          Direction.Est.temp<-Direction_searchtuning_lin(Xc[index.sel,],loading,mu=NULL, resol, maxiter)
-          step.vec[t]<-Direction.Est.temp$step
-        }
-        step<-getmode(step.vec)
-      }
-      Direction.Est<-Direction_fixedtuning_lin(Xc,loading,mu=sqrt(2.01*log(pp)/n)*resol^{-(step-1)})
-      while(is.na(Direction.Est) || length(Direction.Est$proj)==0){
-        step<-step-1
-        Direction.Est <- Direction_fixedtuning_lin(Xc, loading, mu = sqrt(2.01 * log(pp) / n) * resol^{-(step - 1)})
-      }
-      direction<-Direction.Est$proj
-    }
-    correction = t(Xc%*%direction)%*%(y - Xc%*%htheta)/n;
-    debias.est=lasso.plugin+correction*loading.norm
-    se<-sd.est*sqrt(sum((Xc%*%direction)^2)/(n)^2)*loading.norm
-
-    returnList <- list("prop.est" = debias.est,
-                       "se" = se,
-                       "proj"=direction,
-                       "plug.in"=lasso.plugin
-    )
-    return(returnList)
-  }
-}
-
-
 #' Compute Ridge-type weight vector
 #'
 #' @description \eqn{\argmin_{\gamma \in \Delta} \gamma^T (\Gamma + \delta * I) \gamma}.
@@ -192,7 +47,7 @@ opt.weight<-function(Gamma,delta,report.reward=TRUE){
   return(returnList)
 }
 
-#' Bias correction for initial estimator of Gamma target
+#' Bias correction for initial estimator of Gamma when covariate shifts
 #'
 #' @param plug.in Initial estimator of Gamma target, of dimension \eqn{L} x \eqn{L}
 #' @param X.l Design matrix of label \eqn{l} in training data, of dimension \eqn{n_l} x \eqn{p}
@@ -222,7 +77,7 @@ Gamma.shift<-function(plug.in,X.l,X.k,omega.l,omega.k,Y.l,Y.k,Pred.l,Pred.k){
 }
 
 #' Estimate the covariance between the pi(l1,k1) entry and pi(l2,k2) entry
-#' when the covariance for the targeted distribution is unknown
+#' when the covariance for the target distribution is unknown
 #'
 #' @param Var.vec Variance of residuals in groups, of length \eqn{L}
 #' @param l1 Index l1
@@ -262,7 +117,7 @@ cov.inner.shift<-function(Var.vec,l1,k1,l2,k2,X.l1,X.k1,X.l2,X.k2,Pred.mat.targe
 
 
 #' Estimate the covariance between the pi(l1,k1) entry and pi(l2,k2) entry
-#' when the covariance for the targeted distribution is known
+#' when the covariance for the target distribution is known
 #'
 #' @param Var.vec Variance of residuals in groups, of length \eqn{L}
 #' @param l1 Index l1
@@ -294,4 +149,246 @@ cov.inner.shift.known<-function(Var.vec,l1,k1,l2,k2,X.l1,X.k1,X.l2,X.k2,Proj.arr
   }
   var<-var1
   return((var))
+}
+
+Lasso <- function(X, y, lambda = NULL, intercept = TRUE) {
+  p <- ncol(X)
+  n <- nrow(X)
+
+  htheta <- if (is.null(lambda)) {
+    lambda <- sqrt(qnorm(1 - (0.1 / p)) / n)
+    outLas <- slim(X, y, lambda = lambda, method = "lq", q = 2,
+                   verbose = FALSE)
+    # Objective : sqrt(RSS/n) + lambda * penalty
+    c(as.vector(outLas$intercept), as.vector(outLas$beta))
+  } else if (lambda == "CV") {
+    outLas <- cv.glmnet(X, y, family = "gaussian", alpha = 1,
+                        intercept = intercept)
+    # Objective : 1/2 * RSS/n + lambda * penalty
+    as.vector(coef(outLas, s = outLas$lambda.1se))
+  } else if (lambda == "CV.min") {
+    outLas <- cv.glmnet(X, y, family = "gaussian", alpha = 1,
+                        intercept = intercept)
+    # Objective : 1/2 * RSS/n + lambda * penalty
+    as.vector(coef(outLas, s = outLas$lambda.min))
+  } else if (lambda == "scalreg") {
+    Xc <- if (intercept) {
+      cbind(rep(1, n), X)
+    } else {
+      X
+    }
+    outLas <- scalreg(Xc, y)
+    # return object
+    if (intercept) {
+      outLas$coefficients
+    } else {
+      # add a coefficient for the (not estimated) intercept b/c of implementation
+      c(0, outLas$coefficients)
+    }
+  } else {
+    outLas <- glmnet(X, y, family = "gaussian", alpha = 1,
+                     intercept = intercept)
+    # Objective : 1/2 * RSS/n + lambda * penalty
+    as.vector(coef(outLas, s = lambda))
+  }
+
+  if (intercept == TRUE) {
+    return(htheta)
+  } else {
+    return(htheta[2:(p+1)])
+  }
+}
+
+getmode <- function(v) {
+  tbl <- table(v)
+  if (all(tbl == 1)) {
+    median(v)
+  } else {
+    as.numeric(names(which.max(tbl)))
+  }
+}
+
+Direction_fixedtuning_lin<-function(X,loading,mu=NULL){
+  pp<-ncol(X)
+  n<-nrow(X)
+  if(is.null(mu)){
+    mu<-sqrt(2.01*log(pp)/n)
+  }
+  loading.norm<-sqrt(sum(loading^2))
+  if (loading.norm==0){
+    H <- cbind(loading, diag(1, pp))
+  }else{
+    H <- cbind(loading / loading.norm, diag(1, pp))
+  }
+  v<-Variable(pp+1)
+  obj<-1/4*sum((X%*%H%*%v)^2)/n+sum((loading/loading.norm)*(H%*%v))+mu*sum(abs(v))
+  prob<-Problem(Minimize(obj))
+  result<-solve(prob)
+  opt.sol<-result$getValue(v)
+  cvxr_status<-result$status
+  direction<-(-1)/2*(opt.sol[-1]+opt.sol[1]*loading/loading.norm)
+  returnList <- list("proj"=direction)
+  return(returnList)
+}
+
+Direction_searchtuning_lin<-function(X,loading,mu=NULL, resol = 1.5, maxiter = 10){
+  pp<-ncol(X)
+  n<-nrow(X)
+  tryno = 1;
+  opt.sol = rep(0,pp+1);
+  lamstop = 0;
+  cvxr_status = "optimal";
+  mu = sqrt(2.01*log(pp)/n);
+  #mu.initial= mu;
+  while (lamstop == 0 && tryno < maxiter){
+    ###### This iteration is to find a good tuning parameter
+    lastv = opt.sol;
+    lastresp = cvxr_status;
+    loading.norm<-sqrt(sum(loading^2))
+    if (loading.norm==0){
+      H <- cbind(loading, diag(1, pp))
+    }else{
+      H <- cbind(loading / loading.norm, diag(1, pp))
+    }
+    v<-Variable(pp+1)
+    obj<-1/4*sum((X%*%H%*%v)^2)/n+sum((loading/loading.norm)*(H%*%v))+mu*sum(abs(v))
+    prob<-Problem(Minimize(obj))
+    result<-solve(prob)
+    cvxr_status<-result$status
+
+    if(tryno==1){
+      if(cvxr_status=="optimal"){
+        incr = 0;
+        mu=mu/resol;
+        opt.sol<-result$getValue(v) ### we should move this line from above to here
+        temp.vec<-(-1)/2*(opt.sol[-1]+opt.sol[1]*loading/loading.norm)
+        initial.sd<-sqrt(sum((X%*% temp.vec)^2)/(n)^2)*loading.norm ##what's this?
+        temp.sd<-initial.sd
+      }else{
+        incr = 1;
+        mu=mu*resol;
+      }
+    }else{
+      if(incr == 1){ ### if the tuning parameter is increased in the last step
+        if(cvxr_status=="optimal"){
+          lamstop = 1;
+          opt.sol<-result$getValue(v)
+        }else{
+          mu=mu*resol;
+        }
+      }else{
+        if(cvxr_status=="optimal"&&temp.sd<3*initial.sd){ ##Why this condition on sd?
+          mu = mu/resol;
+          opt.sol<-result$getValue(v)
+          temp.vec<-(-1)/2*(opt.sol[-1]+opt.sol[1]*loading/loading.norm)
+          temp.sd<-sqrt(sum((X%*% temp.vec)^2)/(n)^2)*loading.norm
+          #print(temp.sd)
+        }else{
+          mu=mu*resol;
+          opt.sol=lastv;
+          lamstop=1;
+          tryno=tryno-1
+        }
+      }
+    }
+    tryno = tryno + 1;
+  }
+  direction<-(-1)/2*(opt.sol[-1]+opt.sol[1]*loading/loading.norm)
+  step<-tryno-1
+  returnList <- list("proj"=direction,
+                     "step"=step)
+  return(returnList)
+}
+
+index.map<-function(L,l,k){
+  return((2*L-k)*(k-1)/2+l)
+}
+
+proj.direction<-function(Xc,loading,maxiter=6,resol=1.25){
+  n<-dim(Xc)[1]
+  p<-dim(Xc)[2]
+  loading.norm<-sqrt(sum(loading^2))
+  sigma.hat <- (1/n)*(t(Xc)%*%Xc);
+  if ((n>=6*p)){
+    tmp <- eigen(sigma.hat)
+    tmp <- min(tmp$values)/max(tmp$values)
+  }else{
+    tmp <- 0
+  }
+  if ((n>=6*p)&&(tmp>=1e-4)){
+    direction <- solve(sigma.hat)%*%loading
+  }else{
+    step.vec<-rep(NA,3)
+    for(t in 1:3){
+      index.sel<-sample(1:n,size=ceiling(0.5*min(n,p)), replace=FALSE)
+      Direction.Est.temp<-Direction_searchtuning_lin(Xc[index.sel,],loading,mu=NULL, resol, maxiter)
+      step.vec[t]<-Direction.Est.temp$step
+    }
+    step<-getmode(step.vec)
+    Direction.Est<-Direction_fixedtuning_lin(Xc,loading,mu=sqrt(2.01*log(p)/n)*resol^{-(step-1)})
+    while(is.na(Direction.Est) || length(Direction.Est$proj)==0){
+      step<-step-1
+      Direction.Est <- Direction_fixedtuning_lin(Xc, loading, mu = sqrt(2.01 * log(p) / n) * resol^{-(step - 1)})
+    }
+
+    direction<-loading.norm*Direction.Est$proj
+  }
+  return(direction)
+}
+
+#' Decide ridge penalty data-dependently
+#'
+#' @param Gamma Weight Matrix
+#' @param step_delta The step size of searching delta (default = 0.1)
+#' @param MAX_iter Maximum of iterations for searching
+#' @param verbose Print information about delta and reward (default = \code{FALSE})
+#'
+#' @return
+#' \item{delta}{The data-dependent ridge penalty}
+decide_delta <- function(Gamma, step_delta=0.1, MAX_iter=100, verbose=FALSE){
+  L = dim(Gamma)[1]
+  min_eigen = min(eigen(Gamma)$values)
+  max_eigen = max(eigen(Gamma)$values)
+  solution0 = opt.weight(Gamma, 0)
+  reward0 = solution0$reward
+  delta_min = 0.1 * reward0 * (2 * L) / (L - 1)
+  if(delta_min > 2){
+    if(verbose) print(paste("delta path starts from", round(delta_min, 4), "which exceeds our maximum limit 2"))
+    delta = 2
+  }else{
+    delta = delta_min
+    solution = opt.weight(Gamma, delta)
+    reward = solution$reward
+    i = 1
+    while(reward >= 0.9 * reward0){
+      delta_new = delta + step_delta
+      solution = opt.weight(Gamma, delta_new)
+      reward = solution$reward
+      if(reward <= 0.9 * reward0) break
+      if(delta_new > 2){
+        warning(paste("The picked delta is over our maximum limit 2. Early Stopping at iteration", i))
+        break
+      }
+      delta = delta_new
+      i = i+1
+      if((i %% 10 == 0)&verbose){
+        print(paste("Iteration ", i, "delta =", round(delta, 4), "reward ratio = ", round(reward/reward0, 4)))
+      }
+      if(i >= MAX_iter){
+        warning("Delta searching stops, because it reach the Max iterations.")
+        break
+      }
+    }
+  }
+
+  if((min_eigen + delta) < 0.5) warning("Fail to find a suitable delta, the estimator may be not stable enough.")
+
+  solution = opt.weight(Gamma, delta)
+  reward = solution$reward
+  if(verbose){
+    print(paste0("The picked delta is ", round(delta,4)))
+    print(paste0("Reward Ratio is ", round(reward / reward0, 4)))
+    print(paste0("Minimum Eigenvalue plus delta = ", round(min_eigen + delta, 4)))
+  }
+  return(delta)
 }
